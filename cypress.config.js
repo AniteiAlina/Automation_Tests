b4796651a44b24a87ebdcaa2c8e17438432cead0
AniteiAlina <131695035+AniteiAlina@users.noreply.github.com>
2023-05-24 18:11:33 +0300

    Added reports configuration and package.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,22 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
-  maxDuration: 3000,
+  reporter: "cypress-mochawesome-reporter",
+  video: false,
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "Reports",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
+      allureWriter(on, config);
+      return config;
     },
   },
 });
